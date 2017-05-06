424501167f1372360ff87578591ff5075f55b1e0
dbelliss <daveybelliss@yahoo.com>
2017-05-06 02:24:06 -0700

    Fixing accidental overwriting of player movement

---
+++ Assets/Davey/Scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+	void Awake() {
+		GameManager.player = this.gameObject;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Thomas/Scripts/PlayerMovement.cs
+++ Assets/Thomas/Scripts/PlayerMovement.cs
@@ -3,90 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    // Components
-    private Rigidbody playerRB;
-    private Camera playerCam;
+	// Components
+	private Rigidbody playerRB;
+	private Camera playerCam;
 
-    // Movement speed
-    private float movementSpeed = 10.0f;
-    private float jumpSpeed = 100.0f;
+	// Movement control
+	private float movementSpeed = 10.0f;
+	private float jumpSpeed = 300.0f;
+	private float sphereCastDist = 0.7f;
+	private float spherecastSize = 0.4f;
 
-    // Player look
-    public float mouseSensitivity = 100.0f;
-    public float clampAngle = 80.0f;
-    private float rotY = 0.0f;
-    private float rotX = 0.0f;
+	// Player look
+	public float mouseSensitivity = 100.0f;
+	public float clampAngle = 80.0f;
+	private float rotY = 0.0f;
+	private float rotX = 0.0f;
 
-	void Awake() {
-		GameManager.player = this.gameObject;
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody>();
+		playerCam = Camera.main;
+
+		// Get starting rotation
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
 	}
-    // Use this for initialization
-    void Start () {
-        playerRB = GetComponent<Rigidbody>();
-        playerCam = Camera.main;
 
-        // Get starting rotation
-        Vector3 rot = transform.localRotation.eulerAngles;
-        rotY = rot.y;
-        rotX = rot.x;
-    }
-	
 	// Update is called once per frame
 	void Update ()
-    {
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+	{
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
-    void FixedUpdate()
-    {
-        PlayerMove();
-        PlayerJump();
-    }
+	void FixedUpdate()
+	{
+		PlayerMove();
+		PlayerJump();
+	}
 
-    void LateUpdate()
-    {
-        PlayerLook();
-    }
+	void LateUpdate()
+	{
+		PlayerLook();
+	}
 
-    void PlayerMove()
-    {
-        float forwardMovement = Input.GetAxis("Vertical");
-        float sideMovement = Input.GetAxis("Horizontal");
-        Vector3 movement;
+	void PlayerMove()
+	{
+		float forwardMovement = Input.GetAxis("Vertical");
+		float sideMovement = Input.GetAxis("Horizontal");
+		Vector3 movement;
 
-        movement = playerCam.transform.forward * forwardMovement + playerCam.transform.right * sideMovement;
-        movement = movement.normalized * movementSpeed; // Make vector in magnitude of 1 and apply speed
-        movement.y = playerRB.velocity.y;   // Restore gravity to movement
+		movement = playerCam.transform.forward * forwardMovement + playerCam.transform.right * sideMovement;
+		movement = movement.normalized * movementSpeed; // Make vector in magnitude of 1 and apply speed
+		movement.y = playerRB.velocity.y;   // Restore gravity to movement
 
-        // Apply movement
-        playerRB.velocity = movement;
-    }
+		// Apply movement
+		playerRB.velocity = movement;
+	}
 
-    void PlayerLook()
-    {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = -Input.GetAxis("Mouse Y");
+	void PlayerLook()
+	{
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = -Input.GetAxis("Mouse Y");
 
-        // Get the difference per tick in mouse movement
-        rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+		// Get the difference per tick in mouse movement
+		rotY += mouseX * mouseSensitivity * Time.deltaTime;
+		rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-        // Lock the up and down look
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+		// Lock the up and down look
+		rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
 
-        // Apply total difference to local rotation
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+		// Apply total difference to local rotation
+		Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
 
-        // Assign to player's camera
-        playerCam.transform.rotation = localRotation;
-    }
+		// Assign to player's camera
+		playerCam.transform.rotation = localRotation;
+	}
 
-    void PlayerJump()
-    {
-        if (Input.GetButton("Jump"))
-        {
-            playerRB.AddForce(Vector3.up * jumpSpeed);
-        }
-    }
+	void PlayerJump()
+	{
+		Ray groundRay = new Ray(transform.position, Vector3.down);
+		if (Physics.SphereCast(groundRay, spherecastSize, sphereCastDist))
+		{
+			if (Input.GetButton("Jump"))
+			{
+				playerRB.AddForce(Vector3.up * jumpSpeed);
+			}
+		}
+	}
 }
