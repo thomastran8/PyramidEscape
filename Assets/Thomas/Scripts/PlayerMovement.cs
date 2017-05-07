c4eababa485a4e7a752c6068d00d0cb2757fc501
Thomas Tran <thomastran878@gmail.com>
2017-05-07 15:43:23 -0700

    Added potion to player, added mummy to dungeon_test

--- Assets/Thomas/Scripts/PlayerMovement.cs
+++ Assets/Thomas/Scripts/PlayerMovement.cs
@@ -143,7 +143,6 @@ public class PlayerMovement : MonoBehaviour {
 
     void PlayerCrouchCrawl()
     {
-        Debug.Log(initialCamPivotHeight * crouchHeight);
         // Crouch
         if ((Input.GetButtonDown("Crouch") && !isCrouched && !isCrawl))
         {
