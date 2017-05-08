735ebb7567c6754ba92f0f7700b897efbf161786
Davey <daveybelliss@gmail.com>
2017-05-08 03:40:28 -0700

    Moving scripts

---
+++ Assets/Davey/Scripts/CameraShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    private Vector3 originalPosition;
+
+
+    private void Start() {
+        originalPosition = transform.localPosition;
+    }
+    public void StartShake(float intensity, float decreaseFactor) {
+        StartCoroutine(Shake(intensity, decreaseFactor));
+    }
+
+    IEnumerator Shake(float intensity, float decreaseFactor) {
+        float shake = 1;
+        while (shake >= 0) {
+            this.transform.localPosition = originalPosition + (Random.insideUnitSphere * intensity);
+            shake -= Time.deltaTime * decreaseFactor;
+            yield return new WaitForSeconds(.1f);
+        }
+        this.transform.localPosition = originalPosition;
+       yield break;
+
+    }
+}
