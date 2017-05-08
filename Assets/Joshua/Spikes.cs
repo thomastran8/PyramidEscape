0a8b614af3ee1e7b8797e33073315ad539642323
Joshua Heideman <jcheideman@ucdavis.edu>
2017-05-08 01:04:15 -0700

    Added Spikes

---
+++ Assets/Joshua/Spikes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+    public int damage = 1;
+    public Collider SpikeCollid;
+    protected GameObject player;
+    // Use this for initialization
+    void Start () {
+        SpikeCollid = GetComponent<Collider>();
+        player = GameManager.player;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    }
+    void OnTriggerEnter(Collider SpikeCollid)
+    {
+        GameManager.player.SendMessage("applyDamage", damage);
+    }
+}
