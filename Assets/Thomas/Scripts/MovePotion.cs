958d0f97e8e0036cf31eac2188f7ebe5ab7ef7b5
Thomas Tran <thomastran878@gmail.com>
2017-05-07 17:33:22 -0700

    Player can throw potions

---
+++ Assets/Thomas/Scripts/MovePotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePotion : MonoBehaviour {
+    private Rigidbody potionRB;
+    private Transform playerTr;
+    private float throwForce = 1000.0f;
+    private float deleteTimer = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+        potionRB = GetComponent<Rigidbody>();
+        potionRB.AddForce(playerTr.forward * throwForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deleteTimer -= Time.deltaTime;
+        if (deleteTimer <= 0)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Thomas/Scripts/ThrowPotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPotion : MonoBehaviour {
+    private Animator playerAnimator;
+    public GameObject potion;
+    private GameObject rightHand;
+
+	// Use this for initialization
+	void Start () {
+        playerAnimator = GetComponent<Animator>();
+        rightHand = GameObject.Find("PlayerRightHand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            playerAnimator.SetTrigger("ThrowPotion");
+            StartCoroutine(WaitToThrow());
+        }
+	}
+
+    IEnumerator WaitToThrow()
+    {
+        yield return new WaitForSeconds(0.15f);
+        Instantiate(potion, rightHand.transform.position, Quaternion.identity);
+    }
+}
