7e43e8f0bc077e0f949efe82835c4698234837f1
dbelliss <daveybelliss@yahoo.com>
2017-05-08 21:34:14 -0700

    Axe sound

--- Assets/Davey/Scripts/Weapon_Mummy.cs
+++ Assets/Davey/Scripts/Weapon_Mummy.cs
@@ -42,8 +42,13 @@ public class Weapon_Mummy : Mummy {
         if (weaponType == "Axe") {
             int attackNum = Random.Range(0, 2);
             anim.SetTrigger("Atack_" + attackNum.ToString());
-            audios[2].pitch = Random.Range(0f, 1f);
+            audios[2].pitch = Random.Range(.5f, 1f);
             audios[2].Play();
+			audios[6].Play();
+			Debug.Log (audios [5].name);
+			if (attackNum == 1) {
+				audios[6].Play();
+			}
         }
 
     }
