0c13410577dc50a948c9858f60a96e40447eef99
Thomas Tran <thomastran878@gmail.com>
2017-05-08 19:43:12 -0700

    Player can now interact with lever

---
+++ Assets/Thomas/Scripts/PlayerInteract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+    private Camera playerCam;
+    private float interactRange = 1.5f;
+    private Animator playerAnim;
+
+	// Use this for initialization
+	void Start () {
+        playerCam = Camera.main;
+        playerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit interactInfoRay;
+        if (Input.GetButtonDown("Fire2") && Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out interactInfoRay, interactRange))
+        {
+            LeverActivate lever = interactInfoRay.collider.GetComponent<LeverActivate>();
+            if (lever)
+            {
+                lever.ActivateLever();
+                playerAnim.SetTrigger("ActivateObject");
+            }
+        }
+	}
+}
