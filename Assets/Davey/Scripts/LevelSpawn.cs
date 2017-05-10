d1557552196853c7c7a0fab3e9d065d634dd3eb9
Thomas Tran <thomastran878@gmail.com>
2017-05-09 17:52:47 -0700

    Startpoint gets position on awake instead of start

--- Assets/Davey/Scripts/LevelSpawn.cs
+++ Assets/Davey/Scripts/LevelSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelSpawn : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		GameManager.setSpawn (this.transform.position);
 	}
 
