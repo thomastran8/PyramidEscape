093175469ff241857a63019c96e185e99d4134ae
Thomas Tran <thomastran878@gmail.com>
2017-05-09 18:58:02 -0700

    Timed explosion radius collider with animation

--- Assets/Thomas/Scripts/DestroyParticles.cs
+++ Assets/Thomas/Scripts/DestroyParticles.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class DestroyParticles : MonoBehaviour {
     private ParticleSystem ps;
     private AudioSource[] sounds;
+    private SphereCollider explosionRadius;
+    private float destroyExplosionTimer = 0.6f;
 	// Use this for initialization
 	void Start () {
         ps = GetComponent<ParticleSystem>();
         sounds = GetComponents<AudioSource>();
+        explosionRadius = GetComponent<SphereCollider>();
      
         sounds[0].Play();
 
@@ -16,6 +19,11 @@ public class DestroyParticles : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        destroyExplosionTimer -= Time.deltaTime;
+        if (destroyExplosionTimer <= 0)
+        {
+            Destroy(explosionRadius);
+        }
 		if (!ps.IsAlive() && !sounds[0].isPlaying)
         {
             Destroy(gameObject);
