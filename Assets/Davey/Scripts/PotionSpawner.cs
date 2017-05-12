107a24d5ae58ae8b5d5daa1a643adceef87a5b5f
Davey <daveybelliss@gmail.com>
2017-05-11 23:55:07 -0700

    Kindof boss level

---
+++ Assets/Davey/Scripts/PotionSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionSpawner : MonoBehaviour {
+    public GameObject potion;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnPotion());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnPotion() {
+        while (true) {
+            Instantiate(potion, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
