95482519bb989647d1ae7847788d103dee42f06c
Thomas Tran <thomastran878@gmail.com>
2017-05-12 04:48:55 -0700

    Scene mostly complete except minimal treasure room

---
+++ Assets/Thomas/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour {
+    private static GameObject instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this || SceneManager.GetActiveScene().name == this.name)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this.gameObject;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
