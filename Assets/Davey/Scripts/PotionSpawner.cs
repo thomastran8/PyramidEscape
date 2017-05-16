a3ee529cd728801c1d7aef4fc0ad59fb6c2c80a0
Davey <daveybelliss@gmail.com>
2017-05-16 03:34:20 -0700

    Adding potion spawner to last level.

--- Assets/Davey/Scripts/PotionSpawner.cs
+++ Assets/Davey/Scripts/PotionSpawner.cs
@@ -6,8 +6,12 @@ public class PotionSpawner : Activatable {
     public GameObject potion;
 	public float secondsPerPotion;
 	public bool startActive = false;
+    private int numSpawned;
+    public int maxSpawned = 5;
+    List<GameObject> spawned;
 	// Use this for initialization
 	void Start () {
+        spawned = new List<GameObject>();
 		if (startActive) {
 			StartCoroutine (SpawnPotion ());
 		}
@@ -15,7 +19,7 @@ public class PotionSpawner : Activatable {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        removeTaken();
 	}
 
 	public override void activate() {
@@ -26,9 +30,22 @@ public class PotionSpawner : Activatable {
 		StopCoroutine (SpawnPotion ());
 	}
 
+    void removeTaken() {
+        for (int i = 0; i < spawned.Count; i++) {
+            if (spawned[i] == null) {
+                numSpawned--;
+                spawned.RemoveAt(i);
+            }
+        }
+    }
+
     IEnumerator SpawnPotion() {
         while (true) {
-            Instantiate(potion, transform.position, Quaternion.identity);
+            if (numSpawned < maxSpawned) {
+                spawned.Add(Instantiate(potion, transform.position, Quaternion.identity));
+                numSpawned++;
+            }
+
 			yield return new WaitForSeconds(secondsPerPotion);
         }
     }
