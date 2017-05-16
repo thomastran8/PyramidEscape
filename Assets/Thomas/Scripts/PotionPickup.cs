1a8a9575b9de091464fd68da7c2064348e15df32
Thomas Tran <thomastran878@gmail.com>
2017-05-15 20:32:39 -0700

    Potion pickup sound added

--- Assets/Thomas/Scripts/PotionPickup.cs
+++ Assets/Thomas/Scripts/PotionPickup.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PotionPickup : MonoBehaviour {
+    private PlayerUI pUI;
+    private AudioSource audsrc;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        pUI = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerUI>();
+        audsrc = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void pickupPotion()
+    {
+        pUI.getPotion();
+        if (pUI.curPotions > 0)
+        {
+            ThrowPotion.PotionHold.SetActive(true);
+        }
+        Destroy(gameObject);
+    }
 }
